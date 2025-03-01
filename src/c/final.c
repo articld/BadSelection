fe614506edeb84e8181045c787685edaef2d79e3
Enrico Mazzon <enricomazzon@live.it>
2025-03-01 13:17:05 +0100

    Added font file, defined custom font in main

--- src/c/final.c
+++ src/c/final.c
@@ -3,13 +3,20 @@
 static Window *s_window;
 static TextLayer *s_text_layer;
 
+static GFont s_time_font;
+
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+
+  //the font size is probably right +-1
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IBM_BOLD_35));
+
+  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 50));
+  text_layer_set_font(s_text_layer, s_time_font);
+  text_layer_set_text(s_text_layer, "10:10");
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentLeft);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
 
