747031f755a02c7f95ca4305edada4ff33128706
Enrico Mazzon <enricomazzon@live.it>
2025-03-01 12:57:54 +0100

    Cleaned up unnecessary functions, some name changes, created a gitignore file

--- src/c/final.c
+++ src/c/final.c
@@ -3,25 +3,7 @@
 static Window *s_window;
 static TextLayer *s_text_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
-}
-
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
-}
-
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
-}
-
-static void prv_window_load(Window *window) {
+static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
@@ -31,30 +13,27 @@ static void prv_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
 
-static void prv_window_unload(Window *window) {
+static void main_window_unload(Window *window) {
   text_layer_destroy(s_text_layer);
 }
 
-static void prv_init(void) {
+static void init(void) {
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
+
   window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
+    .load = main_window_load,
+    .unload = main_window_unload,
   });
   const bool animated = true;
   window_stack_push(s_window, animated);
 }
 
-static void prv_deinit(void) {
+static void deinit(void) {
   window_destroy(s_window);
 }
 
 int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
+  init();
   app_event_loop();
-  prv_deinit();
+  deinit();
 }
