8a81ac2ed45bf97bfbcbcf7d15c4834702fa32c0
Enrico Mazzon <enricomazzon@live.it>
2025-03-16 12:16:54 +0100

    Currently bugged, created animations.c to put the animation code in.

--- src/c/final.c
+++ src/c/final.c
@@ -1,4 +1,6 @@
 #include <pebble.h>
+#include "animations.h"
+
 #define _NUM_ELEM_ 60
 
 #if defined (PBL_COLOR)
@@ -24,171 +26,15 @@ typedef struct{
     TextLayer *hours, *minutes, *separator;
 
 } TL;
+
 static TL s_TimeLayer;
 
 static Layer *s_TimeLayer_bg;
 
 static TextLayer *s_textgrid_elements[_NUM_ELEM_];
 
-static int s_animation_counter = 0;
-
 static GFont s_time_font, s_textgrid_font;
 //static GFont s_date_font;
-
-static void shuffle_grid_text(){
-    static char s_str[_NUM_ELEM_][2] = {{'a', '\0'}};
-
-    for(int i = 0; i<_NUM_ELEM_; i++){
-        s_str[i][0] = rand() % 26 + 97;
-        text_layer_set_text(s_textgrid_elements[i], s_str[i]);
-    }
-}
-
-//TEXTGRID ANIMATION SHENENIGANS
-//-----------------------------------------------------------------------------
-
-static void anim_started_handler(Animation *animation, void *context) {
-    return;
-}
-
-static void anim_stopped_handler(Animation *animation, bool finished, void *context) {
-    static char s_str[_NUM_ELEM_][2] = {{'a', '\0'}};
-    s_str[s_animation_counter][0] = rand() % 26 + 97;
-
-    text_layer_set_text(context, s_str[s_animation_counter]);
-
-    s_animation_counter = (s_animation_counter + 1) % 60;
-}
-
-static void anim_1_started_handler(Animation *animation, void *context) {
-    return;
-}
-
-static void anim_1_stopped_handler(Animation *animation, bool finished, void *context) {
-    return;
-}
-
-static void animate_grid(){
-    if(!s_animation_counter){
-        const int array_length = _NUM_ELEM_ * 2;
-        Animation **arr = (Animation**)malloc(array_length * sizeof(Animation*));
-
-        const int duration_ms = 300;
-        int delay_ms = 0;
-
-        for(int i=0; i< array_length; i++){
-            if(i % 2 == 0){
-                int j = i / 2;
-
-                GRect start = layer_get_frame(text_layer_get_layer(s_textgrid_elements[j])); 
-                GRect finish = layer_get_frame(text_layer_get_layer(s_textgrid_elements[j]));
-                finish.origin.y = finish.origin.y + finish.size.h;
-                finish.size.h = 0;
-
-                PropertyAnimation *prop_anim = property_animation_create_layer_frame(text_layer_get_layer(s_textgrid_elements[j]), &start, &finish);
-                Animation *anim = property_animation_get_animation(prop_anim);
-                if(!layer_get_hidden(text_layer_get_layer(s_textgrid_elements[j])))
-                    delay_ms += 75;
-
-                animation_set_curve(anim, AnimationCurveEaseIn);
-                animation_set_delay(anim, delay_ms);
-                animation_set_duration(anim, duration_ms);
-
-                animation_set_handlers(anim, (AnimationHandlers){
-                    .started = anim_started_handler,
-                    .stopped = anim_stopped_handler
-                }, s_textgrid_elements[j]);
-
-                arr[i] = anim;
-            }
-
-            else{
-                int j = i - 1;
-                Animation *anim_1 = animation_clone(arr[j]);
-                animation_set_reverse(anim_1, true);
-                animation_set_delay(anim_1, delay_ms + duration_ms + 120);
-                animation_set_handlers(anim_1, (AnimationHandlers){
-                    .started = anim_1_started_handler,
-                    .stopped = anim_1_stopped_handler
-                }, NULL);
-
-                arr[i] = anim_1;
-            }
-        }
-
-        Animation *spawn = animation_spawn_create_from_array(arr , array_length);
-        animation_schedule(spawn);
-
-        free(arr);
-    }
-
-}
-
-//HOUR AND MINUTE ANIMATIONS
-//-----------------------------------------------------------------------------
-
-static void time_anim_started_handler(Animation *animation, void *context){
-    return;
-}
-
-static void time_anim_stopped_handler(Animation *animation, bool finished, void *context){
-    TextLayer *target = (TextLayer *) context;
-
-    if(target == s_TimeLayer.minutes){
-        time_t temp = time(NULL);
-        struct tm *tick_time = localtime(&temp);
-
-        static char s_m_buffer[4];
-        strftime(s_m_buffer, sizeof(s_m_buffer), "%M", tick_time);
-        text_layer_set_text(target, s_m_buffer); 
-    }
-
-    else{
-        time_t temp = time(NULL);
-        struct tm *tick_time = localtime(&temp);
-
-        static char s_h_buffer[4];
-        strftime(s_h_buffer, sizeof(s_h_buffer), clock_is_24h_style() ? "%H" : "%I", tick_time);
-        text_layer_set_text(target, s_h_buffer); 
-    } 
-}
-
-static void animate_time(TextLayer *target, int additional_delay){
-    Animation **arr = (Animation**)malloc(2 * sizeof(Animation*));
-
-    const int duration_ms = 300;
-    const int delay_ms = (75 * _NUM_ELEM_ / 2) + additional_delay;
-
-    Layer *time_layer = text_layer_get_layer(target);
-
-    GRect start = layer_get_frame(time_layer);
-    GRect finish = layer_get_frame(time_layer);
-    finish.origin.y = finish.origin.y + finish.size.h;
-    finish.size.h = 0;
-
-    PropertyAnimation *prop_anim = property_animation_create_layer_frame(time_layer, &start, &finish);
-    Animation *anim = property_animation_get_animation(prop_anim);
-
-    animation_set_curve(anim, AnimationCurveEaseIn);
-    animation_set_delay(anim, delay_ms);
-    animation_set_duration(anim, duration_ms);
-
-    Animation *anim_1 = animation_clone(anim);
-    animation_set_reverse(anim_1, true);
-    animation_set_delay(anim_1, delay_ms + duration_ms + 120);
-
-    animation_set_handlers(anim, (AnimationHandlers){
-        .started = time_anim_started_handler,
-        .stopped = time_anim_stopped_handler
-    }, target);
-
-    arr[0] = anim;
-    arr[1] = anim_1;
-
-    Animation *spawn = animation_spawn_create_from_array(arr, 2);
-    animation_schedule(spawn);
-    free(arr);
-}
 
 //TIMEKEEPING
 //-----------------------------------------------------------------------------
@@ -207,7 +53,7 @@ static void update_time(){
 }
 
 static void update_with_anim(bool first_update){
-    if(!first_update) animate_grid();
+    if(!first_update) animate_grid(s_textgrid_elements);
     animate_time(s_TimeLayer.minutes, 100);
 
     time_t temp = time(NULL);
@@ -258,6 +104,16 @@ static void tick_minute_handler(struct tm *tick_time, TimeUnits units_changed){
 
 //LAYER CREATION
 //-----------------------------------------------------------------------------
+
+static void shuffle_grid_text(){
+    static char s_str[_NUM_ELEM_][2] = {{'a', '\0'}};
+
+    for(int i = 0; i<_NUM_ELEM_; i++){
+        s_str[i][0] = rand() % 26 + 97;
+        text_layer_set_text(s_textgrid_elements[i], s_str[i]);
+    }
+}
+
 static void create_text_grid(Layer *target){
     int x_coord = 0;
     int y_coord = 0;
